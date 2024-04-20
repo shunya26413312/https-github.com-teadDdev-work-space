b22afff5a933c0dc3ef69cc092f3bb61556b84ee
yata78 <tatsuya20000923@icloud.com>
2024-04-20 16:31:50 +0900

    移動系関数の作成

--- main.js
+++ main.js
@@ -9,3 +9,62 @@ document.addEventListener('DOMContentLoaded', () => {
 	let nextGridSquares = Array.from({length: 16}, () => document.createElement('div'));
 	nextGridSquares.forEach(square => nextGrid.appendChild(square))
 });
+
+//ボタン押下時のアクション定義
+document.addEventListener('keydown', function(event) {
+    if (event.key == 'ArrowLeft') {
+        moveLeft();
+    } else if (event.key == 'ArrowRight') {
+        moveRight();
+    } else if (event.key == 'ArrowUp') {
+        rotate();
+    } else if (event.key == 'ArrowDown') {
+        moveDown();
+    }
+});
+
+function moveLeft() {
+    //TODO テトロミノを消す処理
+    //undraw()
+
+    //左端にない場合は左に動く
+    const isAtLeft = current.some(index => ((currentPosition + index) % width === 0))
+    if(!isAtLeft) {
+        currentPosition -= 1
+    }
+
+    //動いた先にテトロミノがある場合は戻る
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition += 1
+    }
+
+    //TODO テトロミノを再描画
+    //draw()
+}
+
+function moveRight() {
+    //TODO テトロミノを消す処理
+    //undraw()
+
+    //右端にない場合は右に動く
+    const isAtRight = current.some(index => ((currentPosition + index) % width === width - 1))
+    if(!isAtRight) {
+        currentPosition += 1
+    }
+
+    //動いた先にテトロミノがある場合は戻る
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition -= 1
+    }
+    
+    //TODO テトロミノを再描画
+    //draw()
+}
+
+function moveDown() {
+    //TODO テトロミノを消す処理
+    //undraw()
+    currentPosition += width;
+    //TODO テトロミノを再描画
+    //draw()
+}
